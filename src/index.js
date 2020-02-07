2d9249044080a456bfb3049eda0e1b4ecc31d1dc
N120 <n12o.dev@gmail.com>
2020-02-07 19:57:36 +0200

    feature 6. Display a draw message when no one wins

--- src/index.js
+++ src/index.js
@@ -82,6 +82,8 @@ function Game() {
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
+  } else if (stepNumber >= 9) {
+    status = "It's a draw";
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
